2856da84c9aa8cae49d777bfac5f8c250a6d4389
Lipika <lipikapande@gmail.com>
2025-02-08 14:20:51 +0530

    challenge 1

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-'use strict';
+// 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// console.log(Person.prototype);
+// console.log(jonas);
+// console.log(jonas.__proto__);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species);
+
+// CHALLENGE 1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate();
