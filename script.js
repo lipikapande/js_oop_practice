02634f83df8a542a4fd0fb30e8d646962e2dae35
Lipika <lipikapande@gmail.com>
2025-02-12 23:22:45 +0530

    prac1

--- script.js
+++ script.js
@@ -36,10 +36,50 @@
 
 //ES6 CLASSES
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+// }
+// const Jessica = new PersonCl('Jessica', 1996);
+
+//OBJECT.CREATE
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+
+//CHALLENGE 2
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(this.speed);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(this.speed);
   }
 }
-const Jessica = new PersonCl('Jessica', 1996);
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes');
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
