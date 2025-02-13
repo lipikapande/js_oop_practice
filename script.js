1482651686eac005b894ff7b1d9c70fb28d5f504
Lipika <lipikapande@gmail.com>
2025-02-13 10:26:15 +0530

    challenges 2 and 3

--- script.js
+++ script.js
@@ -56,30 +56,53 @@
 
 //CHALLENGE 2
 
-class Car {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
-  accelerate() {
-    this.speed += 10;
-    console.log(this.speed);
-  }
-  brake() {
-    this.speed -= 5;
-    console.log(this.speed);
-  }
-}
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(this.speed);
+//   }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(this.speed);
+//   }
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUs(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
 
-const car1 = new Car('BMW', 120);
-const car2 = new Car('Mercedes');
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes');
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(this.speed);
-};
+//CHALLENGE 3
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(this.speed);
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
